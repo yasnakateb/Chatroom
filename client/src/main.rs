09251f5fc3d344f04a215faecbd19832a3b5257d
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 14:30:50 +0430

    Added: thread

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,6 +3,7 @@
 //                  Client                  //
 //                                          //
 //////////////////////////////////////////////
+
 use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
@@ -26,5 +27,66 @@ fn fn main()
     // Instantiate channel and assign it to a string type
     // We are going to be sending a bunch of strings through channel
     let (sender, receiver) = mpsc::channel::<String>();
+
+    // Spawn a thread and create a move closure inside of it with a loop
+
+    thread::spawn(move || loop 
+    {
+        // Create a mutable buffer with a vector with zeros inside of it 
+        let mut buffer = vec![0; MESSAGE_SIZE];
+        // Read our message through the buffer
+        match client.read_exact(&mut buffer) 
+        {
+            Ok(_) => 
+            {
+                // Let message equal our buffer 
+                // Turn it into an iterator 
+                // Check to see if the references inside of it are equal to 0 
+                // Collect all of them inside of our vector
+                // All the ones that are equal to 0 are going to just discard
+                let message = buffer.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("Message recv{:?}",  message);
+            },
+            /* 
+             * If the type of error is equal to an error that would block our non-blocking,
+             * we just send back a unit type. 
+             */ 
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            // If we get an error we don't care about what's inside of it 
+            // We just close the connection and then we just break out of this loop
+            Err(_) => 
+            {
+                println!("Connection with server was severed");
+                break;
+            }
+        }
+        match receiver.try_recv() 
+        {
+            Ok(message) => 
+            {
+                // Clone message into bytes
+                // Put it inside of a buffer variable
+                let mut buffer = message.clone().into_bytes();
+                // Resize our buffer by our message size
+                buffer.resize(MESSAGE_SIZE, 0);
+                // Write all of our buffers into our client
+                client.write_all(&buffer).expect("Writing to socket failed");
+                // Print out the message
+                println!("Message sent {:?}", msg);
+            },
+            /* 
+             * Check if our try receive error is empty and 
+             * if it is then we're just going to send back a unit type
+             */
+            Err(TryRecvError::Empty) => (),
+            /*
+             * Check if it's a disconnected type 
+             * in which case we just want to break the loop
+             */
+            Err(TryRecvError::Disconnected) => break
+        }
+        // Have our thread sleep for a hundred milliseconds
+        thread::sleep(Duration::from_millis(100));
+    }    
 
 }
