342de61ea4cdd9fe48dd3ef8116e67c517f45521
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 10:41:54 +0430

    Added: comment

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,15 @@
+//////////////////////////////////////////////
+//                                          //
+//                  Server                  //
+//                                          //
+//////////////////////////////////////////////
+
 use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
+
+
 
 
 
