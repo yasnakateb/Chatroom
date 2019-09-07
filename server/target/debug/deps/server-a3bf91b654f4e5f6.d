f15b2481fd26bd9839be95a0be1f7e1c2bc8f708
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 14:48:15 +0430

    Typo

---
+++ server/target/debug/deps/server-a3bf91b654f4e5f6.d
@@ -0,0 +1,5 @@
+/Users/yasna/Work/PROJECTS/Chat_App/server/target/debug/deps/server-a3bf91b654f4e5f6: src/main.rs
+
+/Users/yasna/Work/PROJECTS/Chat_App/server/target/debug/deps/server-a3bf91b654f4e5f6.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/Users/yasna/Work/PROJECTS/Chat_App/server/target/debug/server.dSYM: /Users/yasna/Work/PROJECTS/Chat_App/server/src/main.rs
