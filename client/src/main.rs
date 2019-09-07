d04bcd105cad2105992d92260dd6e311381170b5
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 13:51:48 +0430

    Added: imports

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,3 +3,8 @@
 //                  Client                  //
 //                                          //
 //////////////////////////////////////////////
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
