01af4b70e8dc8f311ea0dc936fc4b576edadbf8c
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 14:49:44 +0430

    Added: .gitignore

--- server/target/debug/deps/server-a3bf91b654f4e5f6.d
+++
@@ -1,5 +0,0 @@
-/Users/yasna/Work/PROJECTS/Chat_App/server/target/debug/deps/server-a3bf91b654f4e5f6: src/main.rs
-
-/Users/yasna/Work/PROJECTS/Chat_App/server/target/debug/deps/server-a3bf91b654f4e5f6.d: src/main.rs
-
-src/main.rs:
