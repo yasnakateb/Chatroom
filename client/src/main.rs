f91f6f22488b4f733ed3dd9ecdf69e47996e3405
Yasna Kateb <yasnakatebzadeh@gmail.com>
2019-09-07 13:53:23 +0430

    Added: localhost, port

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,3 +8,6 @@ use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
+
+// Localhost with a port in it
+const LOCAL_HOST: &str = "127.0.0.1:8080";
